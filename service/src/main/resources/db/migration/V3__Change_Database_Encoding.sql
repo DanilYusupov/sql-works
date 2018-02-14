dc0b338d62af97227fb7e27584ca537806576808
Danil Yusupov <danilka5318rus@gamil.com>
2018-02-14 16:22:04 +0300

    Realized read. Set encoding

---
+++ service/src/main/resources/db/migration/V3__Change_Database_Encoding.sql
@@ -0,0 +1,1 @@
+UPDATE pg_database SET encoding = pg_char_to_encoding('UTF8') WHERE datname = 'postgres';
---
+++ web/src/main/java/com/sqlworks/web/WebLogger.java
@@ -0,0 +1,7 @@
+package com.sqlworks.web;
+
+import org.apache.log4j.Logger;
+
+public interface WebLogger {
+    public static Logger log = Logger.getLogger(WebLogger.class);
+}
